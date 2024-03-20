924cb8515b936da53848e2162e90bd1e2e54620b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-20 17:07:13 +0800

    feat(tw): add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,4 +5,24 @@ export default {
     experimental: { optimizeUniversalDefaults: true },
     content: ['./src/**/*.{css,html,js,svelte,ts}'],
     plugins: [typo],
+    theme: {
+        extend: {
+            colors: {
+                'devcamp-red': '#f96743',
+                'devcamp-gray': {
+                    '50': '#f6f6f6',
+                    '100': '#e7e7e7',
+                    '200': '#d1d1d1',
+                    '300': '#b0b0b0',
+                    '400': '#888888',
+                    '500': '#6d6d6d',
+                    '600': '#5d5d5d',
+                    '700': '#4f4f4f',
+                    '800': '#454545',
+                    '900': '#3d3d3d',
+                    '950': '#2a2a2a',
+                },
+            },
+        },
+    },
 } satisfies Config;
