de75d7b1f6b24a13a209275c3cc3e69114ed238b
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-20 17:49:41 +0800

    feat: implement podium component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
 <script>
     import Card from './Card.svelte';
+    import Podium from './Podium.svelte';
 </script>
 
-<Card id={39114273} user="BastiDood" name="Basti Ortiz" score={500} />
-<Card id={98273014} user="Anjellyrika" name="Jelly Raborar" score={69420} />
+<main class="space-y-4">
+    <section class="flex justify-center gap-8">
+        <Podium id={39114273} user="BastiDood" name="Basti Ortiz" score={500} />
+        <Podium id={39114273} user="BastiDood" name="Basti Ortiz" score={500} />
+        <Podium id={98273014} user="Anjellyrika" name="Jelly Raborar" score={69420} />
+    </section>
+    <section class="space-y-2">
+        <Card id={39114273} user="BastiDood" name="Basti Ortiz" score={500} />
+        <Card id={98273014} user="Anjellyrika" name="Jelly Raborar" score={69420} />
+    </section>
+</main>
---
+++ src/routes/Podium.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    // eslint-disable-next-line init-declarations
+    export let id: number;
+    // eslint-disable-next-line init-declarations
+    export let user: string;
+    // eslint-disable-next-line init-declarations
+    export let name: string;
+    // eslint-disable-next-line init-declarations
+    export let score: number;
+</script>
+
+<div class="inline-block space-y-2 text-center">
+    <img
+        src="https://avatars.githubusercontent.com/u/{id}"
+        alt={name}
+        class="aspect-square size-28 rounded-full border-4 border-devcamp-red"
+    />
+    <div class="leading-3">
+        <p class="text-lg text-gray-50">{name}</p>
+        <p class="text-sm text-gray-300">
+            <a target="_blank" href="https://github.com/{user}" class="hover:underline">{user}</a>
+        </p>
+    </div>
+    <span class="font-sans text-xl font-bold text-devcamp-red">{score}</span>
+</div>
