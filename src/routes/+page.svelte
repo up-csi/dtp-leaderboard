359aa663e1e5f8a08d259207511dadebf19d943f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-20 17:16:21 +0800

    chore: add more test instances

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
 </script>
 
 <Card id={39114273} user="BastiDood" name="Basti Ortiz" score={500} />
+<Card id={98273014} user="Anjellyrika" name="Jelly Raborar" score={69420} />
--- src/routes/Card.svelte
+++ src/routes/Card.svelte
@@ -9,7 +9,7 @@
     export let score: number;
 </script>
 
-<div class="grid grid-cols-[auto_auto_1fr_auto] items-center gap-2.5 rounded bg-devcamp-gray-900 p-4">
+<div class="grid grid-cols-[auto_auto_1fr_auto] items-center gap-2.5 rounded-md bg-devcamp-gray-900 p-4 shadow-sm">
     <span>#</span>
     <img src="https://avatars.githubusercontent.com/u/{id}" alt={name} class="aspect-square size-8 rounded" />
     <div class="flex flex-col justify-center">
@@ -18,5 +18,5 @@
             ><a target="_blank" href="https://github.com/{user}" class="hover:underline">{user}</a></span
         >
     </div>
-    <span class="font-bold text-devcamp-red">{score}</span>
+    <span class="font-sans font-bold text-devcamp-red">{score}</span>
 </div>
