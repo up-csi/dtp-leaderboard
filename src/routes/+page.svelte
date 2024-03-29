e94f193931b3a728536350f8969f22b2c2c633b7
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2024-03-29 17:30:04 +0800

    refactor: implement flexible heights for podium

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,27 +9,27 @@
         <h1 class="text-4xl font-bold text-devcamp-red">DevCamp 2024</h1>
         <h2 class="text-2xl">Leaderboard</h2>
     </header>
-    <section class="grid max-h-[80vh] grid-flow-col grid-cols-3 grid-rows-4 gap-x-8">
+    <section class="grid h-[60vh] grid-flow-col grid-cols-3 gap-x-8">
         <!-- 2nd placer -->
-        <div class="row-start-2 self-end">
+        <div class="flex flex-col justify-end">
             <Avatar id={98273014} alt="Anjellyrika" />
-        </div>
-        <div class="row-span-2 bg-devcamp-dark-red p-3">
-            <Profile user="Anjellyrika" name="Jelly Raborar" score={4000} />
+            <div class="h-2/4 bg-devcamp-dark-red p-3">
+                <Profile user="Anjellyrika" name="Jelly Raborar" score={4000} />
+            </div>
         </div>
         <!-- 1st placer -->
-        <div class="row-start-1 self-end">
+        <div class="flex flex-col justify-end">
             <Avatar id={39114273} alt="BastiDood" />
-        </div>
-        <div class="row-span-3 bg-devcamp-dark-red p-3">
-            <Profile user="BastiDood" name="Basti Ortiz" score={40000} />
+            <div class="h-full bg-devcamp-dark-red p-3">
+                <Profile user="BastiDood" name="Basti Ortiz" score={40000} />
+            </div>
         </div>
         <!-- 3rd placer -->
-        <div class="row-start-3 self-end">
+        <div class="flex flex-col justify-end">
             <Avatar id={39114273} alt="BastiDood" />
-        </div>
-        <div class="row-span-1 bg-devcamp-dark-red p-3">
-            <Profile user="BastiDood" name="Basti Ortiz" score={400} />
+            <div class="h-1/3 bg-devcamp-dark-red p-3">
+                <Profile user="BastiDood" name="Basti Ortiz" score={400} />
+            </div>
         </div>
     </section>
     <section class="space-y-2">
