35264ed03e27985a4d6cfa5627acbe9ec34a06e0
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-04-02 13:29:48 +0800

    chore: update leaderboards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     import { assert } from '$lib/assert';
     import upcsi from '$lib/assets/upcsi.svg';
 
-    const lastUpdate = new Date(1711742545772);
+    const lastUpdate = new Date(1712035766472);
     const datetime = lastUpdate.toISOString();
     const localeString = lastUpdate.toLocaleString();
 
