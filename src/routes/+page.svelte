81f919834f889a64fbc7fcdf6a87304dd8eac929
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-06-28 00:46:01 +0800

    chore: update score time

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
     import { assert } from '$lib/assert';
     import upcsi from '$lib/assets/upcsi.svg';
 
-    const lastUpdate = new Date(1715191249691);
+    const lastUpdate = new Date(1719506724597);
     const datetime = lastUpdate.toISOString();
     const localeString = lastUpdate.toLocaleString();
 
