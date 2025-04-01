07f8715b49c6cee027a892db6d1425a8c5d898d9
Ehren Castillo <ehrencastillo@gmail.com>
2025-04-02 02:45:01 +0800

    fix: set manifest link relative to base path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script>
     import './app.css';
     import banner from '$lib/assets/banner.png?url';
+    import { base } from '$app/paths';
     import favicon from '$lib/assets/favicon.png?url';
 </script>
 
 <svelte:head>
     <meta property="og:image" content="https://devcamp.up-csi.org{banner}" />
     <link rel="icon" href={favicon} />
+    <link rel="manifest" href="{base}/manifest.json" />
 </svelte:head>
 
 <slot />
